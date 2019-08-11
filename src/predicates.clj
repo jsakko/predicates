252d37a4909fad85dcab26d856d7d9724c7731b5
Janne Sakko <janne.sakko@solita.fi>
2019-08-11 22:38:06 +0300

    test

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,75 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== x n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
+
+(filter (set->predicate #{1 2 3}) [0 2 4 6])
+(filter (set->predicate #{1 2 3 nil}) [0 2 4 6 nil])
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
+
+(filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3])
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (empty? string)
+      (nil? string)
+      (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [x] (has-award? book x)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [filtered (first (filter pred a-seq))]
+    (println filtered)
+    (or (true? filtered) (false? filtered))))
+
+
+
+
+(my-some even? [1 3 5 7])
+(my-some even? [1 3 5 7 8])
+(my-some neg? [1 3 5 0 7 8])    ;=> falsey
+(my-some neg? [1 3 5 0 7 -1 8]) ;=> true
+(my-some neg? [])               ;=> falsey
+(my-some first [[false] [1]])   ;=> 1
+(my-some first [[false] []])    ;=> falsey
+(my-some nil? [1 2])            ;=> falsey
+(my-some nil? [1 nil 2])
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [truthys (map (fn [x] (pred x)) a-seq)]
+    (empty? (filter (fn [x] (not x)) truthys))))
+
+(my-every? pos? [1 2 3 4])   ;=> true
+(my-every? pos? [1 2 3 4 0]) ;=> false
+(my-every? even? [2 4 6])    ;=> true
+(my-every? even? [])         ;=> true
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (integer? (/ n x)))]
+    (not (some pred (range 2 n)))))
+
+(prime? 4) ;=> false
+(prime? 7) ;=> true
+(prime? 10) ;=> false
+(filter prime? (range 2 50))
 ;^^
